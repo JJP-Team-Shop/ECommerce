6318ace766c23be8d7fbb811378580e2315b3914
betheljn <betheljn@jairusdesktop>
2024-02-12 10:37:01 -0500

    new updated branch

---
+++ prisma/migrations/20240210171336_teamshop/migration.sql
@@ -0,0 +1,73 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "address" TEXT,
+    "isAdmin" BOOLEAN NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product" (
+    "id" SERIAL NOT NULL,
+    "productName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cartItems" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "cartId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "cartItems_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cart" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER,
+    "status" TEXT NOT NULL,
+    "totalAmount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE INDEX "cartItems_productId_idx" ON "cartItems"("productId");
+
+-- CreateIndex
+CREATE INDEX "cartItems_cartId_idx" ON "cartItems"("cartId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cart_userId_key" ON "cart"("userId");
+
+-- CreateIndex
+CREATE INDEX "cart_userId_idx" ON "cart"("userId");
+
+-- AddForeignKey
+ALTER TABLE "cartItems" ADD CONSTRAINT "cartItems_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cartItems" ADD CONSTRAINT "cartItems_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cartItems" ADD CONSTRAINT "cartItems_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cart" ADD CONSTRAINT "cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ zserver/db/seed.js
@@ -0,0 +1,84 @@
+const { PrismaClient } = require("@prisma/client");
+const { getPrismaClient } = require("@prisma/client/runtime/library");
+const prisma = new PrismaClient();
+
+async function seed() {
+  console.log("Seeding the database.");
+  try {
+    // Clear the database.
+    await prisma.user.deleteMany({});
+    await prisma.cartItems.deleteMany({});
+    await prisma.cart.deleteMany({});
+    await prisma.product.deleteMany({});
+
+    const product = await prisma.product.create({
+      data: {
+        productName: "Jersey",
+        description: "J.K. Dobbins",
+        size: "Medium",
+        price: 55.0,
+        quantity: 100,
+        image:
+          "https://fanatics.frgimages.com/baltimore-ravens/mens-nike-jk-dobbins-purple-baltimore-ravens-game-jersey_pi3911000_altimages_ff_3911851-1adebcf5ecc70e85f950alt1_full.jpg?_hv=2&w=900",
+      },
+    });
+
+    const cart = await prisma.cart.create({
+      data: {
+        userId: null,
+        status: "active",
+        
+        totalAmount: 0, //add sum function to add total of cart items
+      },
+    });
+
+    const cartItem = await prisma.cartItems.create({
+      data: {
+        product: {
+          connect: {
+            id: product.id,
+          },
+        },
+        quantity: 1,
+        cart: {
+          connect: {
+            id: cart.id,
+          },
+        },
+      },
+    });
+
+    const user = await prisma.user.create({
+      data: {
+        email: "bob@gmail.com",
+        password: "password",
+        firstName: "Bob",
+        lastName: "Job",
+        address: "123 s street",
+        cartItems: {
+          connect: {
+            id: cartItem.id,
+          },
+        },
+        cart: {
+          connect: {
+            id: cart.id,
+          },
+        },
+        isAdmin: false,
+      },
+    });
+
+    console.log("Database is seeded.");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+if (require.main === module) {
+  seed();
+}
+
+module.exports = seed;
